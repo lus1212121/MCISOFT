291e282216b9d2b478119f5946141071bf5ab201
Manuel Cabarcas <cabarcas12144@gmail.com>
2023-07-19 17:59:10 -0500

    Primer commit

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+// require('dotenv').config();
+const connection = require('./connection.json')
+
+const conexion = mysql.createConnection({
+    host: connection.HOST1,
+    user: connection.USER1,
+    password: connection.PASS1,
+    database: connection.DATA1
+})
+
+conexion.connect((error)=>{
+    if(error){
+        console.log('el error de la conexion es:'+error);
+        return
+    }
+    console.log('CONECTADO A MYSQL');
+})
+
+
+module.exports = conexion;
