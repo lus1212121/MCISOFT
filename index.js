aa96e35b2fdb8ffa5b69c512949c61b4131669f8
miguelangel2023 <miguelangel2023@gmail.com>
2023-07-21 13:56:50 -0500

    fix app config

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const constants = require('./src/shared/constants');
 const conexion = require('./config/db');
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Configuración para servir archivos estáticos
 //app.use(express.static('public'));
-
 
 // local host//
 
 app.use('/', require('./routes/index'));
 
-app.listen(80, ()=>{
-    console.log('servicio http');
+app.listen(constants.HTTP_PORT, constants.IP, () => {
+  console.log('servicio http');
 });
 
-app.listen(443, ()=>{
-    console.log('servicio https');
+app.listen(constants.HTTPS_PORT, constants.IP, () => {
+  console.log('servicio https');
 });
